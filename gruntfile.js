d440ff77e9bbb6a784c144ecffb3058694909fa9
Aliaksandr_Boikiu <Aliaksandr_boikiu@epam.com>
2015-03-07 16:34:03 +0300

    [*] Configure grunt

--- gruntfile.js
+++ gruntfile.js
@@ -1,21 +1,30 @@
 module.exports = function(grunt) {
+  require('jit-grunt')(grunt);
+
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          "css/styles.css": "less/styles.less"
+        }
       }
-    }
+    },
+	watch: {
+		styles: {
+			files: ['less/styles.less'],
+			tasks: ['less'],
+			options: {
+				nospawn: true,
+				livereload: true
+			}
+		}
+	}
   });
-
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
-
+  grunt.registerTask('default', ['less', 'watch']);
+	grunt.loadNpmTasks('grunt-contrib-watch');
 };
