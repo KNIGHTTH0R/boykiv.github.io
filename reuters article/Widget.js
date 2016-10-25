038c29f23b0a6b18010fb32c10b75f900a49b03d
boykiv <boykiv@hotmail.com>
2016-10-25 12:41:32 +0300

    New version of article

--- reuters article/Widget.js
+++ reuters article/Widget.js
@@ -31,7 +31,7 @@ lib.properties = {
 
 
 
-(lib.Afterreview17 = function() {
+(lib.Afterreview17pngcopy2 = function() {
 	this.spriteSheet = ss["Widget_atlas_"];
 	this.gotoAndStop(2);
 }).prototype = p = new cjs.Sprite();
@@ -137,30 +137,6 @@ p.nominalBounds = new cjs.Rectangle(0,0,71.4,29.7);
 p.nominalBounds = new cjs.Rectangle(0,0,71.4,29.7);
 
 
-(lib.Symbol3 = function() {
-	this.initialize();
-
-	// Layer 1
-	this.shape = new cjs.Shape();
-	this.shape.graphics.f("#FFFFFF").s().p("A9IJ+IAAz7MA6RAAAIAAT7g");
-	this.shape.setTransform(186.6,63.9);
-
-	this.addChild(this.shape);
-}).prototype = p = new cjs.Container();
-p.nominalBounds = new cjs.Rectangle(0,0,373.2,127.8);
-
-
-(lib.Symbol2 = function() {
-	this.initialize();
-
-	// Layer 1
-	this.instance = new lib.Afterreview20();
-
-	this.addChild(this.instance);
-}).prototype = p = new cjs.Container();
-p.nominalBounds = new cjs.Rectangle(0,0,530,483);
-
-
 (lib.Linktomartian = function() {
 	this.initialize();
 
@@ -252,19 +228,6 @@ p.nominalBounds = new cjs.Rectangle(-35.6,-14.8,71.4,29.7);
 p.nominalBounds = new cjs.Rectangle(-37.7,-14.8,75.5,29.7);
 
 
-(lib.Symbol4 = function() {
-	this.initialize();
-
-	// Layer 1
-	this.instance = new lib.Symbol3("synched",0);
-	this.instance.setTransform(0,0,0.884,0.477,0,0,0,186.6,63.9);
-	this.instance.alpha = 0.012;
-
-	this.addChild(this.instance);
-}).prototype = p = new cjs.Container();
-p.nominalBounds = new cjs.Rectangle(-164.9,-30.4,329.9,60.9);
-
-
 (lib.Symbol1 = function() {
 	this.initialize();
 
@@ -294,18 +257,30 @@ p.nominalBounds = new cjs.Rectangle(-164.9,-30.4,329.9,60.9);
 		
 		this.stop();
 		
-		this.movieClip_2.addEventListener("click", fl_ClickToGoToAndStopAtFrame_14.bind(this));
-		
-		function fl_ClickToGoToAndStopAtFrame_14()
-		{
-			this.gotoAndStop(1);
-		}
 		
 		
 		/* Stop at This Frame
 		The  timeline will stop/pause at the frame where you insert this code.
 		Can also be used to stop/pause the timeline of movieclips.
 		*/
+		
+		
+		/* Click to Go to Frame and Stop
+		Clicking on the specified symbol instance moves the playhead to the specified frame in the timeline and stops the movie.
+		Can be used on the main timeline or on movie clip timelines.
+		
+		Instructions:
+		1. Replace the number 5 in the code below with the frame number you would like the playhead to move to when the symbol instance is clicked.
+		2.Frame numbers in EaselJS start at 0 instead of 1
+		*/
+		
+		
+		this.movieClip_1.addEventListener("click", fl_ClickToGoToAndStopAtFrame_15.bind(this));
+		
+		function fl_ClickToGoToAndStopAtFrame_15()
+		{
+			this.gotoAndStop(1);
+		}
 	}
 	this.frame_1 = function() {
 		/* Click to Go to Frame and Stop
@@ -424,41 +399,30 @@ p.nominalBounds = new cjs.Rectangle(-164.9,-30.4,329.9,60.9);
 	this.movieClip_14.setTransform(392.5,75.4);
 
 	this.movieClip_13 = new lib.Symbol26();
-	this.movieClip_13.setTransform(375.6,210.4);
+	this.movieClip_13.setTransform(375.6,173.2);
 
 	this.movieClip_12 = new lib.Symbol25();
-	this.movieClip_12.setTransform(282.6,210.4);
+	this.movieClip_12.setTransform(282.6,173.2);
 
 	this.movieClip_11 = new lib.Symbol24();
-	this.movieClip_11.setTransform(196.1,210.4);
+	this.movieClip_11.setTransform(196.1,173.2);
 
 	this.movieClip_10 = new lib.Symbol23();
-	this.movieClip_10.setTransform(116.5,210.4);
+	this.movieClip_10.setTransform(116.5,173.2);
 
 	this.movieClip_9 = new lib.Symbol22();
-	this.movieClip_9.setTransform(38.9,210.4);
+	this.movieClip_9.setTransform(38.9,173.2);
 
 	this.timeline.addTween(cjs.Tween.get({}).to({state:[]}).to({state:[{t:this.movieClip_9},{t:this.movieClip_10},{t:this.movieClip_11},{t:this.movieClip_12},{t:this.movieClip_13},{t:this.movieClip_14}]},1).wait(5));
 
 	// Home
-	this.movieClip_2 = new lib.Symbol4();
-	this.movieClip_2.setTransform(185.2,225,1,1.082);
-
-	this.instance = new lib.Symbol2("synched",0);
-	this.instance.setTransform(265,241.5,1,1,0,0,0,265,241.5);
-
 	this.movieClip_1 = new lib.Symbol1();
-	this.movieClip_1.setTransform(185.2,207.5);
+	this.movieClip_1.setTransform(185.2,227.5);
+	this.movieClip_1.alpha = 0.031;
 
-	this.shape = new cjs.Shape();
-	this.shape.graphics.f().s("#888888").ss(1,1,1).p("ANrmtIKoAAIAANbIqoAAMgl9AAAIAAkqANrmtIAANbAxvmtIfaAA");
-	this.shape.setTransform(176.5,220);
+	this.instance = new lib.Afterreview20();
 
-	this.shape_1 = new cjs.Shape();
-	this.shape_1.graphics.f("#000000").s().p("AlTGtIAAtZIKnAAIAANZg");
-	this.shape_1.setTransform(298,220);
-
-	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.shape_1},{t:this.shape},{t:this.movieClip_1},{t:this.instance},{t:this.movieClip_2}]}).to({state:[]},1).wait(5));
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance},{t:this.movieClip_1}]}).to({state:[]},1).wait(5));
 
 	// Chart
 	this.instance_1 = new lib.Afterreview16();
@@ -467,29 +431,28 @@ p.nominalBounds = new cjs.Rectangle(-164.9,-30.4,329.9,60.9);
 
 	// News
 	this.instance_2 = new lib.Afterreview15();
-	this.instance_2.setTransform(0.5,0);
 
-	this.instance_3 = new lib.Afterreview20();
-
-	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance_3},{t:this.instance_2}]}).to({state:[]},3).wait(3));
+	this.timeline.addTween(cjs.Tween.get(this.instance_2).to({_off:true},3).wait(3));
 
 	// Reviews
-	this.instance_4 = new lib.Afterreview17();
+	this.instance_3 = new lib.Afterreview17pngcopy2();
+	this.instance_3.setTransform(1.2,0);
 
-	this.timeline.addTween(cjs.Tween.get(this.instance_4).to({_off:true},4).wait(2));
+	this.timeline.addTween(cjs.Tween.get(this.instance_3).to({_off:true},4).wait(2));
 
 	// Trailer
-	this.instance_5 = new lib.Afterreview18();
+	this.instance_4 = new lib.Afterreview18();
+	this.instance_4.setTransform(1.2,0);
 
-	this.timeline.addTween(cjs.Tween.get(this.instance_5).to({_off:true},5).wait(1));
+	this.timeline.addTween(cjs.Tween.get(this.instance_4).to({_off:true},5).wait(1));
 
 	// Whatch it
-	this.instance_6 = new lib.Afterreview19();
+	this.instance_5 = new lib.Afterreview19();
 
-	this.timeline.addTween(cjs.Tween.get(this.instance_6).wait(6));
+	this.timeline.addTween(cjs.Tween.get(this.instance_5).wait(6));
 
 }).prototype = p = new cjs.MovieClip();
-p.nominalBounds = new cjs.Rectangle(265,241.5,530,483);
+p.nominalBounds = new cjs.Rectangle(265,241.5,531.2,483);
 
 })(lib = lib||{}, images = images||{}, createjs = createjs||{}, ss = ss||{});
 var lib, images, createjs, ss;
