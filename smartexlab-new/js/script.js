e1425a29b3959bcde612046d483398cf4ae2c1bd
boykiv <boykiv@hotmail.com>
2017-03-06 17:25:31 +0300

    Smartexlab update

--- smartexlab-new/js/script.js
+++ smartexlab-new/js/script.js
@@ -1,4 +1,7 @@
-var os = new OnScreen();
+var os = new OnScreen({
+			debounce: 300,
+			container: window
+		});
 
 /*
 	Behaviors
@@ -32,91 +35,92 @@ var os = new OnScreen();
 	endValue
 */
 
-$(function(){
+		$(function(){
+		
+			var khobSelector = ".khob";
+			// activate knob
+			$(khobSelector).each(function(){						
+				$(this).knob({
+					"thickness" : $(this).data("thickness") || 0.17,
+					"fgColor" : $(this).data("fgcolor") || "#0577BD",
+					"readOnly" : true,
+					"step" : 0.0001,
+					"format" : (value) => {
+						return Math.ceil(value) + '%';
+					}
+				});	
+			});					
+			// animation khob
+			os.on('enter', khobSelector, (element) => {					
+					
+					var jqElement = $(element);
+					
+					if(jqElement.data('isNotFirstCall'))
+						return;
+					jqElement.data('isNotFirstCall',true);	
+					
+					var duration = jqElement.data("duration") || 1000,
+					easing = jqElement.data("easing") || "swing",
+					startValue = jqElement.data("startvalue") || 0,
+					endValue = jqElement.data("endvalue") || jqElement.val();
 
-	var khobSelector = ".khob";
-	// activate knob
-	$(khobSelector).each(function(){						
-		$(this).knob({
-			"thickness" : $(this).data("thickness") || 0.17,
-			"fgColor" : $(this).data("fgcolor") || "#0577BD",
-			"readOnly" : true,
-			"format" : (value) => {
-				return value + '%';
-			}
-		});	
-	});					
-	// animation khob
-	os.on('enter', khobSelector, (element) => {					
-			
-			var jqElement = $(element);
-			
-			if(jqElement.data('isNotFirstCall'))
-				return;
-			jqElement.data('isNotFirstCall',true);	
-			
-			var duration = jqElement.data("duration") || 1000,
-			easing = jqElement.data("easing") || "swing",
-			startValue = jqElement.data("startvalue") || 0,
-			endValue = jqElement.data("endvalue") || jqElement.val();
+					$({animatedVal: startValue}).animate({animatedVal: endValue}, {
+						duration: duration,
+						easing: easing,
+						step: function() {
+							jqElement.val(this.animatedVal).trigger("change");
+						}
+					});
 
-			$({animatedVal: startValue}).animate({animatedVal: endValue}, {
-				duration: duration,
-				easing: easing,
-				step: function() {
-					jqElement.val(Math.ceil(this.animatedVal)).trigger("change");
-				}
 			});
-
-	});
-	
-	var progressBarSelector = ".progress-bar";
-	// activate progress bar
-	$(progressBarSelector).each(function(){		
-	
-		$(this).css({
-			"height" : $(this).data("height"),
-			"width" : $(this).data("width")					
-		});	
-		// css idicator
-		$(this).find('.idicator').css({
-				"background-color" : $(this).data("fgcolor")				
-		});	
-		
-	});					
-	// animation progressBar
-	os.on('enter', progressBarSelector, (element) => {					
 			
-		var curProgressBar = $(element);
-		
-		if(curProgressBar.data('isNotFirstCall'))
-			return;
-		curProgressBar.data('isNotFirstCall',true);	
-							
-		var duration = curProgressBar.data("duration") || 1000,
-		easing = curProgressBar.data("easing") || "swing",
-		upperLimit = curProgressBar.data("upperlimit") || 100,
-		startValue = curProgressBar.data("startvalue") || 0,
-		endValue = curProgressBar.data("endvalue") || 100,				 
-		idicator = curProgressBar.find('.idicator'),
-		label = idicator.find('.label');
-		
-		$({animatedVal:startValue}).animate({animatedVal:endValue}, 
-		{
-			duration: duration,
-			easing: easing,					
-			step: function() {					
-				label.html(Math.round(this.animatedVal) + '%');						
-				var curVal = this.animatedVal * 100 / upperLimit;						
-				// if progress bar is vertical
-				if(curProgressBar.hasClass('vertical-pb')) 
-					idicator.css({'height' : curVal + '%'});
-				else // default if progress bar is horizontal	
-					idicator.css({'width' : curVal + '%'});
-			}
+			var progressBarSelector = ".progress-bar";
+			// activate progress bar
+			$(progressBarSelector).each(function(){		
+			
+				$(this).css({
+					"height" : $(this).data("height"),
+					"width" : $(this).data("width")					
+				});	
+				// css idicator
+				$(this).find('.idicator').css({
+						"background-color" : $(this).data("fgcolor")				
+				});	
+				
+			});					
+			// animation progressBar
+			os.on('enter', progressBarSelector, (element) => {					
+					
+				var curProgressBar = $(element);
+				
+				if(curProgressBar.data('isNotFirstCall'))
+					return;
+				curProgressBar.data('isNotFirstCall',true);	
+									
+				var duration = curProgressBar.data("duration") || 1000,
+				easing = curProgressBar.data("easing") || "swing",
+				upperLimit = curProgressBar.data("upperlimit") || 100,
+				startValue = curProgressBar.data("startvalue") || 0,
+				endValue = curProgressBar.data("endvalue") || 100,				 
+				idicator = curProgressBar.find('.idicator'),
+				label = idicator.find('.label');
+				
+				$({animatedVal:startValue}).animate({animatedVal:endValue}, 
+				{
+					duration: duration,
+					easing: easing,					
+					step: function() {					
+						label.html(Math.round(this.animatedVal) + '%');						
+						var curVal = this.animatedVal * 100 / upperLimit;						
+						// if progress bar is vertical
+						if(curProgressBar.hasClass('vertical-pb')) 
+							idicator.css({'height' : curVal + '%'});
+						else // default if progress bar is horizontal	
+							idicator.css({'width' : curVal + '%'});
+					}
+				});
+
+			});
+				
+
 		});
-
-	});
-		
-
-});
