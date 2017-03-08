280606675561949a42bcfcca04a8bfa867145f89
boykiv <boykiv@hotmail.com>
2017-03-08 15:05:17 +0300

    Project adaptivity fix, menu adaptivity and menu stick to top

--- smartexlab-new/js/script.js
+++ smartexlab-new/js/script.js
@@ -1,126 +1,60 @@
-var os = new OnScreen({
-			debounce: 300,
-			container: window
-		});
+$('.menu-btn, .main-nav .overlay').on('click', function() {
 
-/*
-	Behaviors
-	min : min value (default=0)
-	max : max value (default=100)
-	step : step size (default=1)
-	angleOffset : starting angle in degrees (default=0)
-	angleArc : arc size in degrees (default=360)
-	stopper : stop at min & max on keydown/mousewheel (default=true)
-	readOnly : disable input and events (default=false)
-	rotation : direction of progression (default=clockwise) | anticlockwise
+	$('.main-nav').toggleClass('opened');
+	$('.main-nav .overlay').fadeToggle(400);
+});
 
-	UI
-	cursor : display mode “cursor”, cursor size could be changed passing a numeric value to the option, default width is used when passing boolean value “true” (default=gauge)
-	thickness : gauge thickness
-	lineCap : gauge stroke endings (default=butt, round=rounded line endings)
-	width : dial width
-	height : dial height
-	displayInput : display input (default=true)
-	displayPrevious : displays the previous value with transparency (default=false)
-	fgColor : foreground color
-	inputColor : input value (number) color
-	font : font family
-	fontWeight : font weight
-	bgColor : background color
-	
-	animation
-	duration
-	easing
-	startValue
-	endValue
-*/
+var $mainNav = $('.main-nav');
+var $menuLinks = $('.main-nav ul');
+var $logo = $('.logo');
 
-		$(function(){
-		
-			var khobSelector = ".khob";
-			// activate knob
-			$(khobSelector).each(function(){						
-				$(this).knob({
-					"thickness" : $(this).data("thickness") || 0.17,
-					"fgColor" : $(this).data("fgcolor") || "#0577BD",
-					"readOnly" : true,
-					"step" : 0.0001,
-					"format" : (value) => {
-						return Math.ceil(value) + '%';
-					}
-				});	
-			});					
-			// animation khob
-			os.on('enter', khobSelector, (element) => {					
-					
-					var jqElement = $(element);
-					
-					if(jqElement.data('isNotFirstCall'))
-						return;
-					jqElement.data('isNotFirstCall',true);	
-					
-					var duration = jqElement.data("duration") || 1000,
-					easing = jqElement.data("easing") || "swing",
-					startValue = jqElement.data("startvalue") || 0,
-					endValue = jqElement.data("endvalue") || jqElement.val();
+var topOffset = window.pageYOffset ? window.pageYOffset : document.body.scrollTop;
 
-					$({animatedVal: startValue}).animate({animatedVal: endValue}, {
-						duration: duration,
-						easing: easing,
-						step: function() {
-							jqElement.val(this.animatedVal).trigger("change");
-						}
-					});
+function scrollFunction() {
+	topOffset = window.pageYOffset ? window.pageYOffset : document.body.scrollTop;
+	var menuHeight = 58 - topOffset;
+	var menuLinksTop = 15 - topOffset/2;
+	var logoTop = 4 - topOffset/2/15*11;
+	var menuBackOpacity = 0 + topOffset/28;
 
-			});
-			
-			var progressBarSelector = ".progress-bar";
-			// activate progress bar
-			$(progressBarSelector).each(function(){		
-			
-				$(this).css({
-					"height" : $(this).data("height"),
-					"width" : $(this).data("width")					
-				});	
-				// css idicator
-				$(this).find('.idicator').css({
-						"background-color" : $(this).data("fgcolor")				
-				});	
-				
-			});					
-			// animation progressBar
-			os.on('enter', progressBarSelector, (element) => {					
-					
-				var curProgressBar = $(element);
-				
-				if(curProgressBar.data('isNotFirstCall'))
-					return;
-				curProgressBar.data('isNotFirstCall',true);	
-									
-				var duration = curProgressBar.data("duration") || 1000,
-				easing = curProgressBar.data("easing") || "swing",
-				upperLimit = curProgressBar.data("upperlimit") || 100,
-				startValue = curProgressBar.data("startvalue") || 0,
-				endValue = curProgressBar.data("endvalue") || 100,				 
-				idicator = curProgressBar.find('.idicator'),
-				label = idicator.find('.label');
-				
-				$({animatedVal:startValue}).animate({animatedVal:endValue}, 
-				{
-					duration: duration,
-					easing: easing,					
-					step: function() {					
-						label.html(Math.round(this.animatedVal) + '%');						
-						var curVal = this.animatedVal * 100 / upperLimit;						
-						// if progress bar is vertical
-						if(curProgressBar.hasClass('vertical-pb')) 
-							idicator.css({'height' : curVal + '%'});
-						else // default if progress bar is horizontal	
-							idicator.css({'width' : curVal + '%'});
-					}
-				});
+	console.log(topOffset);
 
-			});
-				
+	if (menuHeight >= 28) {
+		$mainNav.css('height', menuHeight+'px')
+			.removeClass('small-menu')
+			.css('background', 'rgba(59, 137, 201,'+menuBackOpacity+')');
+		$menuLinks.css('top', menuLinksTop+'px');
+		$logo.css('margin', logoTop+'px 0');
+		$('.st0').fadeIn(400);
+	} else {
+		$mainNav.css('height','28px')
+		.addClass('small-menu')
+		.css('background', 'rgba(59, 137, 201, 1)');
+		$menuLinks.css('top', '0px');
+		$logo.css('margin', '-7px 0');
+		$('.st0').fadeOut(400);
+	}
+}
 
-		});
+window.onscroll = scrollFunction;
+
+
+// function scrollFunction() {
+// 	topOffset = window.pageYOffset ? window.pageYOffset : document.body.scrollTop;
+// 	var menuHeight = 58 - topOffset;
+// 	if (menuHeight >= 30) {
+// 		mainNav.style.height = menuHeight + 'px';
+// 		mainNav.style.lineHeight = menuHeight + 'px';
+// 		logo.style.marginTop = -10 + topOffset * 0.02 + 'px';
+// 		mainNav.className = "main-nav";
+// 		menuBtn.style.height = menuHeight + 'px';
+// 		logoBlock.style.height = menuHeight + 'px';
+// 	} else {
+// 		mainNav.style.height = '40px';
+// 		mainNav.style.lineHeight = '30px';
+// 		mainNav.className = "main-nav small";
+// 		logo.style.marginTop = -10 + 50 * 0.02 + 'px';
+// 		menuBtn.style.height = '30px';
+// 		logoBlock.style.height = '30px';
+// 	}
+// }
