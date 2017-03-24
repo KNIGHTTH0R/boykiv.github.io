7fca8c08e1f407e8d8c383d80a228e319e68cf24
boykiv <boykiv@hotmail.com>
2017-03-24 15:41:16 +0300

    update

--- smartexlab-new/js/script.js
+++ smartexlab-new/js/script.js
@@ -11,28 +11,33 @@ var $logo = $('.logo');
 var topOffset = window.pageYOffset ? window.pageYOffset : document.body.scrollTop;
 
 function scrollFunction() {
+	var menuMinHeight = 38;
+	var menuColor = '59, 137, 201';
+	var menuColor = '12, 27, 39';
+
 	topOffset = window.pageYOffset ? window.pageYOffset : document.body.scrollTop;
 	var menuHeight = 58 - topOffset;
 	var menuLinksTop = 15 - topOffset/2;
 	var logoTop = 4 - topOffset/2/15*11;
-	var menuBackOpacity = 0 + topOffset/28;
+	var menuBackOpacity = 0 + topOffset/menuMinHeight;
+	var menuBtnTop = 15 - topOffset/38*3;
 
-	console.log(topOffset);
-
-	if (menuHeight >= 28) {
+	if (menuHeight >= menuMinHeight) {
 		$mainNav.css('height', menuHeight+'px')
 			.removeClass('small-menu')
-			.css('background', 'rgba(59, 137, 201,'+menuBackOpacity+')');
+			.css('background', 'rgba('+menuColor+','+menuBackOpacity+')');
 		$menuLinks.css('top', menuLinksTop+'px');
 		$logo.css('margin', logoTop+'px 0');
 		$('.st0').fadeIn(400);
+		$('.menu-btn svg').css('margin-top', menuBtnTop+'px');
 	} else {
-		$mainNav.css('height','28px')
+		$mainNav.css('height', menuMinHeight+'px')
 		.addClass('small-menu')
-		.css('background', 'rgba(59, 137, 201, 1)');
-		$menuLinks.css('top', '0px');
-		$logo.css('margin', '-7px 0');
+		.css('background', 'rgba('+menuColor+', 1)');
+		$menuLinks.css('top', (menuMinHeight-28)/2+'px');
+		$logo.css('margin', '-3px 0');
 		$('.st0').fadeOut(400);
+		$('.menu-btn svg').css('margin-top', '12px');
 	}
 }
 
